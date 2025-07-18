a5adbeacf4280e93efb2cebbc80aae32e3b61dd0
Ratan <ratanrathod870@gmail.com>
2025-07-18 22:59:21 +0530

    added homepage texts

--- app/pages/Homepage.tsx
+++ app/pages/Homepage.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Homepage = () => {
   return (
-    <div className='w-full min-h-screen  '>
-      <h1></h1>
+    <div className='w-full min-h-screen flex mt-54 justify-center '>
+      <div className="text-container h-fit leading-tight">
+        <h1 className='text-7xl'>A place to dump</h1>
+
+        <div className="arrow flex items-center gap-2">
+          <Image src="/Arrow 1.svg" alt="Arrow" width={300} height={10} className="mt-2 w-[25vw]" />
+          <h1 className='text-[7vw] '>Anything</h1>
+        </div>
+
+      </div>
     </div>
   )
 }
